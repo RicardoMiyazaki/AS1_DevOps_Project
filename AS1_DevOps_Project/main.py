111a3242a00fcc4df4da5d426505ac2d667eecda
Ricardo Hideki Miyazaki <70922834+RicardoMiyazaki@users.noreply.github.com>
2024-09-11 11:11:58 -0300

    Projeto Base

---
+++ AS1_DevOps_Project/main.py
@@ -0,0 +1,12 @@
+import requests
+from pprint import pprint
+
+API_Key = 'dab2e4d2d099b06d753e139496a2fb05'
+
+city = input("Digite a Cidade: ")
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_Key
+
+weather_data = requests.get(base_url).json()
+
+pprint(weather_data)
